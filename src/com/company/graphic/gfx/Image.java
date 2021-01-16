b845a50ff6e2d0f7b7ccb786c1a37a4c2de93d3a
Matteo <matteo.naldi1998@gmail.com>
2021-01-16 18:55:00 +0100

    commit 5: images

---
+++ src/com/company/graphic/gfx/Image.java
@@ -0,0 +1,51 @@
+package com.company.graphic.gfx;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Image {
+    private int width;
+    private int height;
+    private boolean alpha = false;
+    private int[] pixels;
+
+    public Image(String path) {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (image != null) {
+            width = image.getWidth();
+            height = image.getHeight();
+            pixels = image.getRGB(0, 0, width, height, null, 0, width);
+            image.flush();
+        }
+    }
+
+    public Image(int[] pixels, int width, int height) {
+        this.pixels = pixels;
+        this.width = width;
+        this.height = height;
+
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public boolean isAlpha() {
+        return alpha;
+    }
+
+    public void setAlpha(boolean alpha) {
+        this.alpha = alpha;
+    }
+}
