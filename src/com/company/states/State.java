4b22e645aeec4a7b7616cdafdfa8dd83c3a733f4
Matteo <matteo.naldi1998@gmail.com>
2021-11-23 17:32:29 +0100

    commit 46: game states

---
+++ src/com/company/states/State.java
@@ -0,0 +1,6 @@
+package com.company.states;
+
+import com.company.graphic.Graphic;
+
+public interface State extends Graphic {
+}
---
+++ src/com/company/states/StateManager.java
@@ -0,0 +1,38 @@
+package com.company.states;
+
+import com.company.graphic.primitives.Stack;
+
+import java.util.HashMap;
+
+public abstract class StateManager {
+
+    private final Stack gameStack;
+
+    private final HashMap<String, State> stateMap;
+
+    public StateManager() {
+        gameStack = new Stack();
+        stateMap = new HashMap<>();
+        insertStates();
+    }
+
+    public State head() {
+        return gameStack.getTop();
+    }
+
+    public void remove() {
+        gameStack.pop();
+    }
+
+    public void insert(String state) {
+        gameStack.push(stateMap.get(state));
+    }
+
+    protected abstract void insertStates();
+
+    protected void addState(String stateName, State state) {
+        stateMap.put(stateName, state);
+    }
+
+    public abstract void init();
+}
